8bd0efcfabe3e323ab6cb98eab13a568d9632f17
Mikkel Kibsgaard Dall <mdall24@student.aau.dk>
2025-12-02 12:56:47 +0100

    Pushed my changes regarding SoMe & Fonts
    updated social media script + added minecraft font :)

--- Assets/Scripts/Social Media/SocialMediaFeed.cs
+++ Assets/Scripts/Social Media/SocialMediaFeed.cs
@@ -4,13 +4,16 @@ public class SocialMediaFeed : MonoBehaviour
 {
     [SerializeField] Scrollable scroll;
     [SerializeField] Canvas feedCanvas;
+    [SerializeField] GameObject postPrefab;
     Transform contentBottom;
-    void SendPost()
+    void SendPost(string text, Texture2D img)
     {
+        Instantiate(postPrefab, feedCanvas.transform);
+        OSMechanics.ResizeImageToSize(img, postPrefab.GetComponent<RectTransform>(), 1f);
 
     }
     Vector2 FindNextPos()
     {
-        return contentBottom.position + Vector3.down*0.1f;
+        return contentBottom.position + Vector3.down * 0.1f;
     }
 }
