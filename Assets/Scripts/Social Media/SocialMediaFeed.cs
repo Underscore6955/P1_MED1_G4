7186c66bccefa7472e4055fef41a436412b97818
Underscore6955 <mikkelrmarquardsen@gmail.com>
2025-12-11 13:29:19 +0100

    Slight scroll change

--- Assets/Scripts/Social Media/SocialMediaFeed.cs
+++ Assets/Scripts/Social Media/SocialMediaFeed.cs
@@ -34,6 +34,7 @@ public class SocialMediaFeed : MonoBehaviour
             curPost.transform.localPosition = new Vector3(curPost.transform.localPosition.x, curPost.transform.localPosition.y, 0);
         }
         scroll.curScroll = scroll.MaxScroll();
+        scroll.ScrollToPos();
     }
     GameObject SendPost((string text, string name, Texture2D pfp, Texture2D img) data)
     {
